b443888f00c9055bac53d51893e7efcd00fb3412
Hissan Ahmad <64192989+Hissan147@users.noreply.github.com>
2025-09-24 15:26:37 +0500

    User Profile Page Changes

--- src/app/mainapp/components/userprofile/lawajam/pricingwidget.ts
+++ src/app/mainapp/components/userprofile/lawajam/pricingwidget.ts
@@ -7,78 +7,89 @@ import { RippleModule } from 'primeng/ripple';
     selector: 'lawajam-pricing',
     imports: [DividerModule, ButtonModule, RippleModule],
     template: `
-        <div id="pricing" class="py-6 px-6 lg:px-20 my-2 md:my-6">
-            <div class="text-center mb-6">
-                <div class="text-surface-900 dark:text-surface-0 font-normal mb-2 text-4xl">Plan Features</div>
-           </div>
-         
-           <div class="grid grid-cols-12 gap-4 justify-between mt-20 md:mt-0">
+        <div id="pricing">
+            <div class="grid grid-cols-12 gap-2 px-0 justify-between mt-10 md:mt-0">
                 <div class="col-span-12 lg:col-span-6 p-0 md:p-4">
-                    <div class="p-4 flex flex-col border-surface-200 dark:border-surface-600 pricing-card cursor-pointer border-2 hover:border-primary duration-300 transition-all" style="border-radius: 10px">
-                        <div class="text-surface-900 dark:text-surface-0 text-center my-8 text-3xl"><p-button icon="pi pi-user"/></div>
-                        <div class="text-center text-3xl"> Individual</div>
-                        <div class="my-8 flex flex-col items-center gap-4">
-                            <div class="flex items-center">
-                                <span class="text-5xl font-bold mr-2 text-surface-900 dark:text-surface-0">$20</span>
-                                <span class="text-surface-600 dark:text-surface-200">per month</span>
-                            </div>
-                            <button pButton pRipple label="Get Started" class="p-button-rounded border-0 ml-4 font-light leading-tight bg-blue-500 text-white"></button>
+                    <div class="flex flex-col border-surface-200 dark:border-surface-600 pricing-card cursor-pointer border-2 hover:border-primary duration-300 transition-all" style="border-radius: 10px">
+                        <div class="flex flex-col items-center gap-4 mt-20 mb-10">
+                            <div class="text-center bold text-xl">Individual</div>
+                            <span class="text-5xl font-bold mr-2 text-surface-900 dark:text-surface-0">$20</span>
+                            <span class="text-surface-600 dark:text-surface-200">Billed Monthly</span>
                         </div>
-                        <p-divider class="w-full bg-surface-200"></p-divider>
-                        <ul class="my-8 list-none p-0 flex text-surface-900 dark:text-surface-0 flex-col px-8">
-                            <li class="py-2">
-                                <i class="pi pi-fw pi-check text-xl text-cyan-500 mr-2"></i>
+                        <ul class="my-8 list-none p-0 flex flex-col px-8 text-gray-900 dark:text-white space-y-4">
+                            <li class="flex items-center">
+                                <span class="flex items-center justify-center w-6 h-6 rounded-full bg-primary text-white mr-3">
+                                    <i class="pi pi-check text-xs"></i>
+                                </span>
                                 <span class="text-xl leading-normal">Responsive Layout</span>
                             </li>
-                            <li class="py-2">
-                                <i class="pi pi-fw pi-check text-xl text-cyan-500 mr-2"></i>
+                            <li class="flex items-center">
+                                <span class="flex items-center justify-center w-6 h-6 rounded-full bg-primary text-white mr-3">
+                                    <i class="pi pi-check text-xs"></i>
+                                </span>
                                 <span class="text-xl leading-normal">Unlimited Push Messages</span>
                             </li>
-                            <li class="py-2">
-                                <i class="pi pi-fw pi-check text-xl text-cyan-500 mr-2"></i>
+                            <li class="flex items-center">
+                                <span class="flex items-center justify-center w-6 h-6 rounded-full bg-primary text-white mr-3">
+                                    <i class="pi pi-check text-xs"></i>
+                                </span>
                                 <span class="text-xl leading-normal">50 Support Ticket</span>
                             </li>
-                            <li class="py-2">
-                                <i class="pi pi-fw pi-check text-xl text-cyan-500 mr-2"></i>
+                            <li class="flex items-center">
+                                <span class="flex items-center justify-center w-6 h-6 rounded-full bg-primary text-white mr-3">
+                                    <i class="pi pi-check text-xs"></i>
+                                </span>
                                 <span class="text-xl leading-normal">Free Shipping</span>
                             </li>
                         </ul>
+                        <div class="flex p-footer bg-gray-100 py-14 mt-6">
+                            <div class="flex flex-row justify-center w-full px-10">
+                                <p-button label="Current Activated Package" severity="contrast" disabled [rounded]="true"></p-button>
+                            </div>
+                        </div>
                     </div>
                 </div>
 
-                <div class="col-span-12 lg:col-span-6 p-0 md:p-6 mt-6 md:mt-0">
-                    <div class="p-4 flex flex-col border-surface-200 dark:border-surface-600 pricing-card cursor-pointer border-2 hover:border-primary duration-300 transition-all" style="border-radius: 10px">
-                        <div class="text-surface-900 dark:text-surface-0 text-center my-8 text-3xl"><p-button icon="pi pi-users"/></div>
-                         <div class="text-center bold text-3xl"> Family</div>
-                        <div class="my-8 flex flex-col items-center gap-4">
-                            <div class="flex items-center">
-                                <span class="text-5xl font-bold mr-2 text-surface-900 dark:text-surface-0">$100</span>
-                                <span class="text-surface-600 dark:text-surface-200">per month</span>
-                            </div>
-                            <button pButton pRipple label="Get Started" class="p-button-rounded border-0 ml-4 font-light leading-tight bg-blue-500 text-white"></button>
+                <div class="col-span-12 lg:col-span-6 p-0 md:p-4">
+                    <div class="flex flex-col border-surface-200 dark:border-surface-600 pricing-card cursor-pointer border-2 hover:border-primary duration-300 transition-all" style="border-radius: 10px">
+                        <div class="flex flex-col items-center gap-4 mt-20 mb-10">
+                            <div class="text-center bold text-xl">Family</div>
+                            <span class="text-5xl font-bold mr-2 text-surface-900 dark:text-surface-0">$100</span>
+                            <span class="text-surface-600 dark:text-surface-200">Billed Monthly</span>
                         </div>
-                        <p-divider class="w-full bg-surface-200"></p-divider>
-                        <ul class="my-8 list-none p-0 flex text-surface-900 dark:text-surface-0 flex-col px-8">
-                            <li class="py-2">
-                                <i class="pi pi-fw pi-check text-xl text-cyan-500 mr-2"></i>
+                        <ul class="my-8 list-none p-0 flex flex-col px-8 text-gray-900 dark:text-white space-y-4">
+                            <li class="flex items-center">
+                                <span class="flex items-center justify-center w-6 h-6 rounded-full bg-primary text-white mr-3">
+                                    <i class="pi pi-check text-xs"></i>
+                                </span>
                                 <span class="text-xl leading-normal">Responsive Layout</span>
                             </li>
-                            <li class="py-2">
-                                <i class="pi pi-fw pi-check text-xl text-cyan-500 mr-2"></i>
+                            <li class="flex items-center">
+                                <span class="flex items-center justify-center w-6 h-6 rounded-full bg-primary text-white mr-3">
+                                    <i class="pi pi-check text-xs"></i>
+                                </span>
                                 <span class="text-xl leading-normal">Unlimited Push Messages</span>
                             </li>
-                            <li class="py-2">
-                                <i class="pi pi-fw pi-check text-xl text-cyan-500 mr-2"></i>
+                            <li class="flex items-center">
+                                <span class="flex items-center justify-center w-6 h-6 rounded-full bg-primary text-white mr-3">
+                                    <i class="pi pi-check text-xs"></i>
+                                </span>
                                 <span class="text-xl leading-normal">50 Support Ticket</span>
                             </li>
-                            <li class="py-2">
-                                <i class="pi pi-fw pi-check text-xl text-cyan-500 mr-2"></i>
+                            <li class="flex items-center">
+                                <span class="flex items-center justify-center w-6 h-6 rounded-full bg-primary text-white mr-3">
+                                    <i class="pi pi-check text-xs"></i>
+                                </span>
                                 <span class="text-xl leading-normal">Free Shipping</span>
                             </li>
                         </ul>
+                        <div class="flex p-footer bg-gray-100 py-14 mt-6">
+                            <div class="flex flex-row justify-center w-full px-10">
+                                <p-button label="Upgrade to Family" severity="contrast" [rounded]="true"></p-button>
+                            </div>
+                        </div>
                     </div>
                 </div>
-
             </div>
         </div>
     `
